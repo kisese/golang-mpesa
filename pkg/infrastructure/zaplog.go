8592589cda749820843fc4f2c31e82efc94e0173
Brian Kisese <brayokisese@gmail.com>
2022-07-15 16:26:02 +0300

    Update zaplog.go

--- pkg/infrastructure/zaplog.go
+++ pkg/infrastructure/zaplog.go
@@ -10,15 +10,10 @@ import (
 var Log *zap.SugaredLogger
 
 func InitLogger() {
-
-	outputPaths := []string{"stdout", utils.GetPath() + "/pkg/storage/log/debug.log"}
-	fmt.Printf("pwd %v  :", utils.GetPath()+"/pkg/storage/log/debug.log")
 	config := zap.NewDevelopmentConfig()
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	config.EncoderConfig.TimeKey = "timestamp"
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.OutputPaths = outputPaths
-	config.ErrorOutputPaths = outputPaths
 	loggerMgr, _ := config.Build()
 
 	zap.ReplaceGlobals(loggerMgr)
